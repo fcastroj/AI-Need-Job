59dff5f3f7fafd3cd1d711173ddd165705a6c3e4
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2025-03-06 13:30:44 -0500

    extract text from an uploaded cv

--- CVapp/forms.py
+++ CVapp/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import CV
+from django.core.exceptions import ValidationError
+import os 
 
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = CV
         fields = ['file']
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+
+        ext = os.path.splitext(file.name)[1].lower()
+
+        valid_extensions = ['.pdf', '.md', '.txt']
+
+        if ext not in valid_extensions:
+            raise ValidationError(f"Solo se permiten archivos con extensiones: {', '.join(valid_extensions)}")
+
+        return file
