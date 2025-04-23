2be37e184fe321a1ff5ea11fb297bb956a4a3b88
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2025-04-23 11:09:26 -0500

    embedding done

--- AiNeedJob/urls.py
+++ AiNeedJob/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from CVapp import views as cvapp_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', cvapp_views.home, name='home'),
+    path('', include('CVapp.urls'), name='jobseeker'),
     path('offer/', include('offer.urls'), name='offer'),
     path('user/', include('users.urls'), name='user'),
 ]
