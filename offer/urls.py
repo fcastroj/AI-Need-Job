a94882640c7d12b53c34533e69a67e13718996cb
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2025-05-08 11:49:58 -0500

    closed and open a published vacancy

--- offer/urls.py
+++ offer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('upload_vacancies/',views.upload_vacancies, name='upload_vacancies'),
     path('upload_cvs/',views.uploadCVS, name='upload_cvs'),
+    path('open_vacancies/<int:vacancy_id>',views.change_state_vacancy, name='change_vacancy_state'),
 ]
